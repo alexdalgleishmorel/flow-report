1ee233cf1703ca59b3a3585f6e176016da53897e
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-02-02 11:49:56 -0700

    prod deploy configurations

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/flow-report/',
   plugins: [
     react(),
     legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
+  ]
 })
