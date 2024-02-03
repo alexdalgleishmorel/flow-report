df574b0163d1fe4824e89d4f7d55e28bb30d42fe
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-02-03 14:51:48 -0700

    added weather data integration

--- src/dataContext.tsx
+++ src/dataContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 
-import DATA from './data.json';
+import FLOW_DATA from './flowData.json';
+import WEATHER_DATA from './weatherData.json';
 
 interface DataContextType {
   flowData: FlowData[];
@@ -22,7 +23,7 @@ export const DataProvider: React.FC<{children: ReactNode}> = ({ children }) => {
   const [stateUpdate, setStateUpdate] = useState<number>(0);
 
   useEffect(() => {
-    setFlowData(processFlowData(DATA));
+    setFlowData(processFlowData(FLOW_DATA, WEATHER_DATA));
   }, []);
 
   useEffect(() => {
@@ -71,22 +72,45 @@ export const useData = () => {
   return context;
 };
 
-function processFlowData(rawData: any): FlowData[] {
+function processFlowData(rawFlowData: any, rawWeatherData: any): FlowData[] {
+  const hourlyTemperatures: number[] = rawWeatherData['hourly']['temperature_2m'].slice(0, 168);
+  const sunriseStrings: string[] = rawWeatherData['daily']['sunrise'].slice(0, 4).map((value: string) => value.split('T')[1]);
+  let sunriseHours: number [] = sunriseStrings.map(string => {
+    let parts = string.split(":");
+    let hour = parseInt(parts[0], 10);
+    let minutes = parseInt(parts[1], 10);
+    
+    return minutes > 30 ? hour + 1 : hour;
+  });
+  let sunsetStrings: string[] = rawWeatherData['daily']['sunset'].slice(0, 4).map((value: string) => value.split('T')[1]);
+  let sunsetHours: number [] = sunsetStrings.map(string => {
+    let parts = string.split(":");
+    let hour = parseInt(parts[0], 10);
+    let minutes = parseInt(parts[1], 10);
+    
+    return minutes > 30 ? hour + 1 : hour;
+  });
+  
   let flowData: FlowData[] = [];
-  rawData?.elements?.forEach((day: any) => {
+  rawFlowData?.elements?.forEach((day: any) => {
     let date: Date = new Date();
     let dataPoints: FlowDataPoint[] = [];
     day.entry?.forEach((entry: any) => {
       const dateTime = entry.period.split(" ", 2);
       const volume = Number(entry.barrier);
-      dataPoints.push({ hour: Number(dateTime[1])-1, volume: volume });
+      dataPoints.push({ hour: Number(dateTime[1])-1, volume: volume, temperature: hourlyTemperatures.shift() });
       date = new Date(`${dateTime[0]}T00:00:00-07:00`);
     });
     let now = new Date();
     now.setHours(0, 0, 0, 0);
     date.setHours(0, 0, 0, 0);
     if (date >= now) {
-      flowData.push({ day: date, dataPoints: dataPoints });
+      flowData.push({
+        day: date, 
+        dataPoints: dataPoints, 
+        sunriseHour: sunriseHours.shift(), 
+        sunsetHour: sunsetHours.shift() }
+      );
     }
   });
   return flowData;
@@ -94,10 +118,13 @@ function processFlowData(rawData: any): FlowData[] {
 
 export interface FlowData {
   day: Date;
+  sunriseHour?: number;
+  sunsetHour?: number;
   dataPoints: FlowDataPoint[];
 };
 
 export interface FlowDataPoint {
   hour: number;
   volume: number;
+  temperature?: number;
 }
