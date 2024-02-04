498f12158e9dc6a070dce8dc425aa4523319077d
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-02-04 00:50:39 +0000

    Updates

--- assets/keyboard2-legacy--mB0GWby.js
+++ assets/keyboard2-legacy--mB0GWby.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-hAX-WW97.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BNzZnD14.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
