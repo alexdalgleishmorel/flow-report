125ae06518aab26910f0651054557ad4692cce2d
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-02-08 17:09:32 -0700

    added summary home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,22 +6,27 @@ import UpperBanner from '../components/banner/UpperBanner';
 import DailyFlowsContainer from '../components/dailyFlowsContainer/DailyFlowsContainer';
 import { useData } from '../dataContext';
 import './Home.css';
+import Summary from '../components/summary/Summary';
 
 
 const Home = () => {
-  const { flowData } = useData();
+  const { flowData, selectedIndex } = useData();
 
   useEffect(() => {
     toggleDarkTheme(isDarkModeDefault());
   }, []);
 
+  const graphView = (
+    <div className='graph-content'>
+      <IonHeader><UpperBanner /></IonHeader>
+      <IonContent scrollY={false} fullscreen><DailyFlowsContainer /></IonContent>
+      <IonFooter><LowerBanner /></IonFooter>
+    </div>
+  );
+
   const content = flowData.length ? (
     <IonPage>
-      <IonHeader><UpperBanner /></IonHeader>
-      <IonContent scrollY={false} fullscreen>
-        <div className='content'><DailyFlowsContainer /></div>
-      </IonContent>
-      <IonFooter><LowerBanner /></IonFooter>
+      {selectedIndex >= 0 ? graphView : <Summary></Summary>}
     </IonPage>
   ) : <Loading></Loading>;
 
@@ -30,7 +35,7 @@ const Home = () => {
 
 function Loading() {
   return (
-    <div className='content'>
+    <div className='full-screen-content'>
       <IonSpinner name='dots'></IonSpinner>
       <IonText color='primary'>Fetching River Data</IonText>
     </div>
