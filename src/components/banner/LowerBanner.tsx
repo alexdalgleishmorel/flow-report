1ef51d8457e89036200b333af57dfe0e4e81dd3b
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-02-09 15:15:15 -0700

    more styling improvements

--- src/components/banner/LowerBanner.tsx
+++ src/components/banner/LowerBanner.tsx
@@ -1,51 +1,58 @@
-import { IonText, IonRange, IonIcon } from "@ionic/react";
-import { chevronBackOutline, chevronForwardOutline, homeOutline } from "ionicons/icons";
+import { IonText, IonIcon } from "@ionic/react";
+import { addOutline, chevronBackOutline, chevronForwardOutline, homeOutline, removeOutline } from "ionicons/icons";
 
 import { useData } from "../../dataContext";
 import './LowerBanner.css';
 
+const MINIMUM_FLOW_RATE = 25;
+const MAXIMUM_FLOW_RATE = 35;
 
 function LowerBanner() {
     const { flowData, targetFlow, selectedIndex, stateUpdate, setTargetFlow, setSelectedIndex, setStateUpdate } = useData();
-
-    const handleSliderChange = (value: number) => {
-        value = !value ? 1 : value;
-        if (value !== targetFlow) {
-            setTargetFlow(value);
-        }
-    };
 
     return (
         <div className='bannerContainer'>
             <div className='titleValueStacked first'>
                 <IonIcon
-                    color="medium"
+                    color="dark"
                     icon={homeOutline} 
-                    size="large"
                     onClick={() => setSelectedIndex(-1) }
                 ></IonIcon>
             </div>
             <div className='titleValueStacked middle'>
-                <IonText color='primary'><b>{targetFlow} m³/s</b></IonText>
-                <div className="rangeContainer">
-                    <IonRange value={targetFlow} min={25} max={35} step={1} onIonInput={(event) => handleSliderChange(+event.detail.value)}></IonRange>
+                <IonText color='medium' className="title">Minimum Flow</IonText>
+                <div className="row">
+                    <IonIcon 
+                        color={targetFlow === MINIMUM_FLOW_RATE ? 'light' : 'dark'}
+                        icon={removeOutline} 
+                        onClick={() => targetFlow !== MINIMUM_FLOW_RATE ? setTargetFlow(targetFlow-1) : setTargetFlow(targetFlow) }
+                    >
+                    </IonIcon>
+                    <div className="small-spacer"></div>
+                    <IonText color='primary' className="flow-value"><b>{targetFlow} m³/s</b></IonText>
+                    <div className="small-spacer"></div>
+                    <IonIcon 
+                        color={targetFlow === MAXIMUM_FLOW_RATE ? 'light' : 'dark'}
+                        icon={addOutline} 
+                        onClick={() => targetFlow !== MAXIMUM_FLOW_RATE ? setTargetFlow(targetFlow+1) : setTargetFlow(targetFlow) }
+                    >
+                    </IonIcon>
                 </div>
             </div>
             <div className='dateContainer last'>
                 <IonText color={'medium'} className='title'>{flowData[selectedIndex].day.toDateString().split(' ').slice(1).join(' ')}</IonText>
+                <div className="small-spacer"></div>
                 <div className="row">
                     <IonIcon 
-                        color={!selectedIndex ? 'light' : 'primary'}
+                        color={!selectedIndex ? 'light' : 'dark'}
                         icon={chevronBackOutline} 
-                        size="large"
                         onClick={() => selectedIndex ? setSelectedIndex(selectedIndex-1) : setSelectedIndex(selectedIndex) }
                     >
                     </IonIcon>
                     <div className="spacer"></div>
                     <IonIcon 
-                        color={!(selectedIndex < flowData.length-1) ? 'light' : 'primary'}
+                        color={!(selectedIndex < flowData.length-1) ? 'light' : 'dark'}
                         icon={chevronForwardOutline} 
-                        size="large"
                         onClick={() => selectedIndex < flowData.length-1 ? setSelectedIndex(selectedIndex+1) : setSelectedIndex(selectedIndex) }
                     >
                     </IonIcon>
