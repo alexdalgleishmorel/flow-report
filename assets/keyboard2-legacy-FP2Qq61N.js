5b16f572102eb3c82f7a3857e55119f02c6e63d6
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-02-10 11:09:07 +0000

    Updates

--- assets/keyboard2-legacy-FP2Qq61N.js
+++ assets/keyboard2-legacy-FP2Qq61N.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-HAoMKj9X.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-qH3udW2r.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
