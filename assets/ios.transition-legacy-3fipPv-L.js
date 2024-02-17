eeb907000d6ff0ce40fa6e0b8852f9fa3b41a5c5
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-02-17 11:09:36 +0000

    Updates

--- assets/ios.transition-legacy-3fipPv-L.js
+++ assets/ios.transition-legacy-3fipPv-L.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-U5b7Yn2m.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-QJ5KUjFN.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
