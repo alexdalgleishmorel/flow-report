b9e50be6749da14cf98af1a616bfd36499c0621b
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-02-25 05:12:26 +0000

    Updates

--- assets/swipe-back-legacy-QeYgX8as.js
+++ assets/swipe-back-legacy-QeYgX8as.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-khqYG5J2.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-1DOd3KQg.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
