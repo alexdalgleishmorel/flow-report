de5f6494a81adc8416651ef3aec0afc0803c4524
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-02-27 19:10:39 +0000

    Updates

--- assets/ios.transition-legacy-jsP-Dfnx.js
+++ assets/ios.transition-legacy-jsP-Dfnx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-bBhUvNjO.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-mT2ew85H.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
