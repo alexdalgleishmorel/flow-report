ac01414a3641c62d02e56111ca2c01fae8a35b37
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-02 17:10:22 +0000

    Updates

--- assets/ios.transition-legacy-caS3DCpD.js
+++ assets/ios.transition-legacy-caS3DCpD.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D_9SU6q0.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-0I-crDuE.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-RYRagFqB.js
+++ assets/swipe-back-legacy-RYRagFqB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D_9SU6q0.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-0I-crDuE.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
