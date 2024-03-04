3d53835009ffbad61293010d54a18258e7b95681
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-04 20:13:06 +0000

    Updates

--- assets/md.transition-legacy-U8Eds-WP.js
+++ assets/md.transition-legacy-U8Eds-WP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fQMeAwDp.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-v-owiE8E.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
