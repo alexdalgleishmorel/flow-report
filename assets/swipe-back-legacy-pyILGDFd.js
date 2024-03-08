d21999e6507fedf199da930592ae2d64e9b7c915
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-08 05:13:23 +0000

    Updates

--- assets/swipe-back-legacy-pyILGDFd.js
+++ assets/swipe-back-legacy-pyILGDFd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-cK1xhcoX.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Va4VTU3o.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
