40020d5965d51834c4ddc19813f1cc3ceafdb0b4
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-09 13:12:14 +0000

    Updates

--- assets/ios.transition-legacy-oymutEba.js
+++ assets/ios.transition-legacy-oymutEba.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fUHk9eDR.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-891cYbWb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
