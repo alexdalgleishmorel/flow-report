5400c21dac844a6e383d71e34debae3b34e7f469
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-10 13:13:18 +0000

    Updates

--- assets/ios.transition-legacy-eBG_3XSR.js
+++ assets/ios.transition-legacy-eBG_3XSR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-r6j6O4vY.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-UoEKo121.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
