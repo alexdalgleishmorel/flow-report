647929fc9f8307f04d452c3210a1395647092272
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-10 18:58:30 -0600

    Updates

--- assets/md.transition-legacy-VgIM3BDs.js
+++ assets/md.transition-legacy-VgIM3BDs.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-kF-1NOTv.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-P5FAUq-h.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
