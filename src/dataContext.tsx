ccce7ce687208ec77e1ef6e4294781e2b874e8ff
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-10 18:32:14 -0600

    removed reliance on isDST

--- src/dataContext.tsx
+++ src/dataContext.tsx
@@ -120,7 +120,6 @@ export const useData = () => {
 };
 
 function processFlowData(rawFlowData: any, rawWeatherData: any): FlowData[] {
-  const isDST: boolean = TIME_DATA['dst'];
   const hourlyTemperatures: number[] = rawWeatherData['hourly']['temperature_2m'].slice(0, 168);
   const sunriseStrings: string[] = rawWeatherData['daily']['sunrise'].slice(0, 4).map((value: string) => value.split('T')[1]);
   let sunriseHours: number [] = sunriseStrings.map(string => {
@@ -145,9 +144,11 @@ function processFlowData(rawFlowData: any, rawWeatherData: any): FlowData[] {
     let dataPoints: FlowDataPoint[] = [];
     day.entry?.forEach((entry: any) => {
       const dateTime = entry.period.split(" ", 2);
+      const yearMonthDay = dateTime[0].split("-", 3);
       const volume = Number(entry.barrier);
       dataPoints.push({ hour: Number(dateTime[1])-1, volume: volume, temperature: hourlyTemperatures.shift() });
-      date = new Date(`${dateTime[0]}T00:00:00-0${isDST ? 6 : 7}:00`);
+      date = new Date();
+      date.setFullYear(yearMonthDay[0], yearMonthDay[1]-1, yearMonthDay[2]);
     });
     let now = new Date();
     now.setHours(0, 0, 0, 0);
