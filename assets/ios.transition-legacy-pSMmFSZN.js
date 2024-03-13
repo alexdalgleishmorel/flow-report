ad7bc5f62595be5eab1349769dad2105e20f4fb3
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-13 18:15:19 +0000

    Updates

--- assets/ios.transition-legacy-pSMmFSZN.js
+++ assets/ios.transition-legacy-pSMmFSZN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-eTDo-GUh.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-zD04KzqI.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
