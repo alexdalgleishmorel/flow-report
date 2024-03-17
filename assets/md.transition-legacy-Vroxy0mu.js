7a41d7fe8b91536585317d3c44d3a1403d41c337
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-17 16:13:55 +0000

    Updates

--- assets/md.transition-legacy-Vroxy0mu.js
+++ assets/md.transition-legacy-Vroxy0mu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-IndlY2qd.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-Sl3rqbmD.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
