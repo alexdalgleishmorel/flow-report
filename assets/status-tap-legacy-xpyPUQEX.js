8df97d752e0f53dcf832f0a1b597ac00145faacf
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-17 05:13:02 +0000

    Updates

--- assets/status-tap-legacy-xpyPUQEX.js
+++ assets/status-tap-legacy-xpyPUQEX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-N6brUm44.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-2rYSFG3h.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
