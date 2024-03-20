98ed790e08e17429b2a244c2c13cb61376530161
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-20 16:16:54 +0000

    Updates

--- assets/md.transition-legacy-10HG-nIM.js
+++ assets/md.transition-legacy-10HG-nIM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-wq_BG2o4.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-bSbiVO_A.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
