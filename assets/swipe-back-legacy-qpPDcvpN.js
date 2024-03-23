df342c40659e9615ed3d1cfc858109100b646bc4
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-23 06:19:44 +0000

    Updates

--- assets/swipe-back-legacy-qpPDcvpN.js
+++ assets/swipe-back-legacy-qpPDcvpN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy--jeHSE2q.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-532K16bp.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
