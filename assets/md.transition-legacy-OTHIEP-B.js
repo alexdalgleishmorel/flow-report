d1cab3d87ad81248854a62f4d3e4643e8f837d25
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-24 06:16:13 +0000

    Updates

--- assets/md.transition-legacy-OTHIEP-B.js
+++ assets/md.transition-legacy-OTHIEP-B.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-86BeD7lT.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-CDooI9tY.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
