02be1bfbe063acbe423c4203a3b75ea4c1b64a16
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-03-28 09:13:33 +0000

    Updates

--- assets/md.transition-legacy-O-_gGPPh.js
+++ assets/md.transition-legacy-O-_gGPPh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-qEzDHVmp.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-OyCoN2zb.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
