2d336114d1166afba036b6bba38679f306a3cad6
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-04-08 01:51:48 +0000

    Updates

--- assets/ios.transition-legacy-pI_cv-Mq.js
+++ assets/ios.transition-legacy-pI_cv-Mq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ZSk2JWOp.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-_yvKikWN.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
