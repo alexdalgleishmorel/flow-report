6c29617b4d5249e616f55b29032a3ab05075ef86
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-04-08 03:14:23 +0000

    Updates

--- assets/ios.transition-legacy-6GK5eeeh.js
+++ assets/ios.transition-legacy-6GK5eeeh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-q83Eerxx.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-lHc8mtIB.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-YbRTLA_U.js
+++ assets/md.transition-legacy-YbRTLA_U.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-q83Eerxx.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-lHc8mtIB.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
