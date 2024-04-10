755f7a59430811cb99400e326e4a6f9201ba062c
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-04-10 00:55:17 +0000

    Updates

--- assets/ios.transition-legacy-WIbUjind.js
+++ assets/ios.transition-legacy-WIbUjind.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-XbsLpsv2.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-vhfBv0dN.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
