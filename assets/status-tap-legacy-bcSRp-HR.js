ded81249c9694396180a37fc1e84c38bb31f730e
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-04-11 04:14:56 +0000

    Updates

--- assets/status-tap-legacy-bcSRp-HR.js
+++ assets/status-tap-legacy-bcSRp-HR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-iDe0aezt.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-zxqWzhOl.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
