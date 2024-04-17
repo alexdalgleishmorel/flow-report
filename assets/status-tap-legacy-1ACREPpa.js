784176346d0881bfe90bb00b0e7a834652e7eb98
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-04-17 06:19:17 +0000

    Updates

--- assets/status-tap-legacy-1ACREPpa.js
+++ assets/status-tap-legacy-1ACREPpa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-wiG56SWq.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-IW8zr6Gy.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
