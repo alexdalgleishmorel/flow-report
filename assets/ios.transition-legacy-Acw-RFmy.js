90b3dda677b5971b176a67f33fb14a6d1fcc38fb
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-04-18 18:16:23 +0000

    Updates

--- assets/ios.transition-legacy-Acw-RFmy.js
+++ assets/ios.transition-legacy-Acw-RFmy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-cKFRzoW7.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-5UBfW_4H.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
