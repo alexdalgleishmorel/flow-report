d0382718bbd95d8c6228754e59ae0895568bcfa0
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-04-23 15:13:26 +0000

    Updates

--- assets/ios.transition-legacy-VRIZ-wLL.js
+++ assets/ios.transition-legacy-VRIZ-wLL.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-IZ7Ue5A2.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-uqSPN-yF.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
