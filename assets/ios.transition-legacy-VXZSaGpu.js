74c9c799321ecac535249480f21c1d60871f1df0
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-05-02 14:12:29 +0000

    Updates

--- assets/ios.transition-legacy-VXZSaGpu.js
+++ assets/ios.transition-legacy-VXZSaGpu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3S4Xl3De.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-MmrEVYnZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
