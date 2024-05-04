0fc7b5b7d05400a34e4c748a89c0a9966a84c922
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-05-04 13:14:14 +0000

    Updates

--- assets/status-tap-legacy-_Wv38-J8.js
+++ assets/status-tap-legacy-_Wv38-J8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-QsK8Bu8E.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-7TY-kmUR.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-cpOhHkHV.js
+++ assets/swipe-back-legacy-cpOhHkHV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-QsK8Bu8E.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-7TY-kmUR.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
