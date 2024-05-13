3c6b7ec2f5ad5076b387f955ebccf82a1fcf4a3b
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-05-13 16:18:27 +0000

    Updates

--- assets/status-tap-udGr74V0.js
+++ assets/status-tap-udGr74V0.js
@@ -1,3 +1,3 @@
-import{r as a,f as i,b as c,w as d,s as l}from"./index-ERs4DTR3.js";/*!
+import{r as a,f as i,b as c,w as d,s as l}from"./index-Qkr4iZ-C.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
