048918a65ed26c308c40ecb2b07de14870c391c6
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-05-17 09:15:44 +0000

    Updates

--- assets/md.transition-legacy-XvdPrcig.js
+++ assets/md.transition-legacy-XvdPrcig.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-HR3Jc--X.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-OUtpqA7p.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
