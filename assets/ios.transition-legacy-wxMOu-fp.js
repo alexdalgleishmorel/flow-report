7a52d098c31ef00a080404a2f797628b412ae9d3
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-05-19 09:14:17 +0000

    Updates

--- assets/ios.transition-legacy-wxMOu-fp.js
+++ assets/ios.transition-legacy-wxMOu-fp.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-RP28OFVr.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-_vE3aJeP.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
