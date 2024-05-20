729075cdb79386c2834b9a040b2e0772085d1925
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-05-20 08:17:48 +0000

    Updates

--- assets/ios.transition-legacy-OFo5DLPT.js
+++ assets/ios.transition-legacy-OFo5DLPT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-a7RPUscm.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-Pky_pBPL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
