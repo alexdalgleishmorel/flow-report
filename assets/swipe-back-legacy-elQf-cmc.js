c4150bccdb1ac875748ca0682280a49b1cc5400a
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-05-21 06:19:10 +0000

    Updates

--- assets/swipe-back-legacy-elQf-cmc.js
+++ assets/swipe-back-legacy-elQf-cmc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-A_-zh6AF.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-kWvCdtOV.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
