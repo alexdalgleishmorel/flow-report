c2b96d8fc83bf921291245fd2bd6acd904d1024c
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-05-28 10:14:45 +0000

    Updates

--- assets/ios.transition-legacy-vepn_pC4.js
+++ assets/ios.transition-legacy-vepn_pC4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-4soITDHg.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-54Ehh5wT.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
