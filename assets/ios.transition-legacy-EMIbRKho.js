c57a9694177be46fa221c512abcc790ecb06c2a2
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-06 10:14:56 +0000

    Updates

--- assets/ios.transition-legacy-EMIbRKho.js
+++ assets/ios.transition-legacy-EMIbRKho.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-6I0kt0rb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-gmL0kZaa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
