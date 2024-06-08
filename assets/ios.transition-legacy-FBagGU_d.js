a38cf852dfefd70562189a30fca58e361fed9866
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-08 11:12:01 +0000

    Updates

--- assets/ios.transition-legacy-FBagGU_d.js
+++ assets/ios.transition-legacy-FBagGU_d.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D_2j63zC.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-UOSCTg0u.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
