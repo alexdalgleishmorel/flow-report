645a2cfbee4e8232232a23c5acbc6ea73a4cb661
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-11 21:13:13 +0000

    Updates

--- assets/ios.transition-legacy-ETmda_7y.js
+++ assets/ios.transition-legacy-ETmda_7y.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-tM9roA9v.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-BhKJ2Qav.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
