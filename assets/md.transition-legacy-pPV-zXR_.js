2174473f5783622124b4eb2938d64755be1e215d
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-11 22:13:14 +0000

    Updates

--- assets/md.transition-legacy-pPV-zXR_.js
+++ assets/md.transition-legacy-pPV-zXR_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BhKJ2Qav.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-8Z_24Z2y.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
