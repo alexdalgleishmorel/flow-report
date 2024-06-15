8f905dfcbe0d917487b5fbd9c57fb99bcfac0a01
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-15 11:12:01 +0000

    Updates

--- assets/ios.transition-legacy-XurbCf71.js
+++ assets/ios.transition-legacy-XurbCf71.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-w0VqXAxD.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-tw9MTgBT.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
