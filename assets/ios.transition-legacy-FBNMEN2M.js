b550dea0d2ec21b7f217186c784ed87d519970c5
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-18 13:16:53 +0000

    Updates

--- assets/ios.transition-legacy-FBNMEN2M.js
+++ assets/ios.transition-legacy-FBNMEN2M.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-eSSUMYOs.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-aYRg8DgE.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
