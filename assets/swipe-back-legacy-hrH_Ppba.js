5dd6df52931916c761611e9c4d883ae8a094cf71
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-20 12:26:12 +0000

    Updates

--- assets/swipe-back-legacy-hrH_Ppba.js
+++ assets/swipe-back-legacy-hrH_Ppba.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Wry09XDU.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-8oJz50oU.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
