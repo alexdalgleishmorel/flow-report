378da1abc3b041eb1bdc35438a3c23bad086ebb8
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-21 07:14:13 +0000

    Updates

--- assets/md.transition-legacy-on_CLoIP.js
+++ assets/md.transition-legacy-on_CLoIP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-E7Ztuoir.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-0yiII9Gg.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
