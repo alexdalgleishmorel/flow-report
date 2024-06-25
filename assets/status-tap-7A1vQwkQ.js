e98f84a8c539f69792615438dcefba558a90f7a8
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-25 12:26:35 +0000

    Updates

--- assets/status-tap-7A1vQwkQ.js
+++ assets/status-tap-7A1vQwkQ.js
@@ -1,3 +1,3 @@
-import{r as a,f as i,b as c,w as d,s as l}from"./index-ny8T6zF4.js";/*!
+import{r as a,f as i,b as c,w as d,s as l}from"./index-anBgo2Ya.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
