85f482ffb755147dda3ca7a3cc4638400631e3b0
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-28 23:14:38 +0000

    Updates

--- assets/md.transition-legacy-T775nrkb.js
+++ assets/md.transition-legacy-T775nrkb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-snbQpcYW.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-j2nHovZQ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
