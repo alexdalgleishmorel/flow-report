fc9f250f0c914b2a8692f91491a2c5184e3c05fc
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-06-30 10:13:41 +0000

    Updates

--- assets/ios.transition-legacy--zJoxlvr.js
+++ assets/ios.transition-legacy--zJoxlvr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Babjh6Kq.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-bDk47aHJ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
