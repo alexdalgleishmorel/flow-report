bca102935388017840c2cb15f201d58c9adfdbe2
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-04 04:16:52 +0000

    Updates

--- assets/md.transition-legacy-10-MzMeN.js
+++ assets/md.transition-legacy-10-MzMeN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-HRMLC8Lw.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-CSR2DzsR.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
