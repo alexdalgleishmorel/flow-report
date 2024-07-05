a1f2a5bd62e73fcf00d0b9bd6a63e403965b786d
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-05 17:13:20 +0000

    Updates

--- assets/ios.transition-legacy-sRTCqcAP.js
+++ assets/ios.transition-legacy-sRTCqcAP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-SNsv0lz9.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-7KgXIwcR.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
