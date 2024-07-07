3b794f70e81943516e5de79763c700b55569ef9a
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-07 07:13:35 +0000

    Updates

--- assets/md.transition-legacy-HOE-z2iD.js
+++ assets/md.transition-legacy-HOE-z2iD.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ATx30Krf.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-L6sh8SB5.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
