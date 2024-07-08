dfb725180d9614434739668e8195310a94a071d0
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-08 11:13:08 +0000

    Updates

--- assets/ios.transition-legacy-BEEIgt32.js
+++ assets/ios.transition-legacy-BEEIgt32.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-gFOq3aSM.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-zmWbHedD.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
