aae98927b0603b5ed70fe3d623910ec7b3c5f876
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-10 01:03:34 +0000

    Updates

--- assets/md.transition-legacy-KKP-WMTg.js
+++ assets/md.transition-legacy-KKP-WMTg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Ritnbo5a.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-mAK52382.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
