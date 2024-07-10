bf52bf3d40525289cdb5c145ac6e8d97dae500d8
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-10 20:14:58 +0000

    Updates

--- assets/ios.transition-legacy-zCPIrfNI.js
+++ assets/ios.transition-legacy-zCPIrfNI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-V1-q26yk.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy--7lLqf23.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
