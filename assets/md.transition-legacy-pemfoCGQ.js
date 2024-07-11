e3c043ab08bee835d5fa990cd380ef8912769021
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-11 11:13:01 +0000

    Updates

--- assets/md.transition-legacy-pemfoCGQ.js
+++ assets/md.transition-legacy-pemfoCGQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-_Efvx0vs.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-WZIixKIN.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
