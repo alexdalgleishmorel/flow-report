088866d5587ace1b47cc99fc39c81610d36bb838
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-12 03:16:38 +0000

    Updates

--- assets/ios.transition-legacy-RsSmswzS.js
+++ assets/ios.transition-legacy-RsSmswzS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-2b8qIHuP.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-jg3GQBKu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
