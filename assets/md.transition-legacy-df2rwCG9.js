4a8e793e51975d3553a122a43ba21dd000cd783c
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-24 01:04:24 +0000

    Updates

--- assets/md.transition-legacy-df2rwCG9.js
+++ assets/md.transition-legacy-df2rwCG9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Nmt0iIIR.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-isQ-Knri.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
