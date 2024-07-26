262fa47bbacb569c47ded53266b5f0f936568162
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-26 22:14:30 +0000

    Updates

--- assets/ios.transition-legacy-zU-wute5.js
+++ assets/ios.transition-legacy-zU-wute5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-iezPU2B5.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-POxhVOsS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
