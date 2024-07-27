11f3d9a72d04dff1f3f0cd971367f11f45b4398a
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-27 17:12:50 +0000

    Updates

--- assets/ios.transition-legacy-FWwDn-DQ.js
+++ assets/ios.transition-legacy-FWwDn-DQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Syf92x6N.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-L5JIaLXu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
