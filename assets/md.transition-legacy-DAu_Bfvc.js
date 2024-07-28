15020459b31059214acbbfdd2a5f97a15f4921fa
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-07-28 09:16:03 +0000

    Updates

--- assets/md.transition-legacy-DAu_Bfvc.js
+++ assets/md.transition-legacy-DAu_Bfvc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-wEFgltKQ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-82XqEayR.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
