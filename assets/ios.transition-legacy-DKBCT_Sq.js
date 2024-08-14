bb810a447863a2dc8d29dfe266d54d760c869a21
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-08-14 13:18:59 +0000

    Updates

--- assets/ios.transition-legacy-DKBCT_Sq.js
+++ assets/ios.transition-legacy-DKBCT_Sq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-RrPr6xiY.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-M_yLHncE.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
