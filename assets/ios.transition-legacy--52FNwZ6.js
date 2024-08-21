0ae9e0935be9de8c45c9e62be745dacfe560774a
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-08-21 03:16:24 +0000

    Updates

--- assets/ios.transition-legacy--52FNwZ6.js
+++ assets/ios.transition-legacy--52FNwZ6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-4MNO7l0P.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-MrwfcLIa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-cqi9jqWP.js
+++ assets/status-tap-legacy-cqi9jqWP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-4MNO7l0P.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-MrwfcLIa.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
