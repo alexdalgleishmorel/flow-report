f01d5dc646d038ebb03c3dec238cf1dc48a59017
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-08-26 10:16:43 +0000

    Updates

--- assets/ios.transition-legacy-EpnOSRP_.js
+++ assets/ios.transition-legacy-EpnOSRP_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-P5Lpfmz3.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-_Euus-fT.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
