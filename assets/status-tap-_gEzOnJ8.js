2bbec960ff6e051ec63f4efa3b651810a57c0cd7
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-08-26 12:28:44 +0000

    Updates

--- assets/status-tap-_gEzOnJ8.js
+++ assets/status-tap-_gEzOnJ8.js
@@ -1,3 +1,3 @@
-import{r as a,f as i,b as c,w as d,s as l}from"./index-Cix71nP-.js";/*!
+import{r as a,f as i,b as c,w as d,s as l}from"./index-71wluwBI.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
--- assets/swipe-back-legacy-bzcLRVUH.js
+++ assets/swipe-back-legacy-bzcLRVUH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-nKf-s8Lm.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-QvyL_fT6.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
