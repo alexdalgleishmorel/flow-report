1ee48dd58687a998f0f8ff286d540820fb5853f1
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-08-27 22:15:30 +0000

    Updates

--- assets/md.transition-legacy-popm98k_.js
+++ assets/md.transition-legacy-popm98k_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-YdIq_0Gr.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-GwDc4pgg.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
