f62b36692fa066e8c8bd59aa2f1e2112775f3a19
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-08-28 04:18:36 +0000

    Updates

--- assets/md.transition-legacy-SkFaoPo3.js
+++ assets/md.transition-legacy-SkFaoPo3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-lBOqe_9p.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy--zsh5QfT.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
