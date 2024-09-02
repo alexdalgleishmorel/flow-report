6801d0471937436803ca16ce0e561a7b1a6d2bd1
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-09-02 11:13:03 +0000

    Updates

--- assets/status-tap-legacy-JK50MBIo.js
+++ assets/status-tap-legacy-JK50MBIo.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-h3_pt18K.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-XEgtT52J.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
