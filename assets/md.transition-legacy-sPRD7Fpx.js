d618d8b4c87cc680e6d184b443a90bfb511c5825
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-09-10 05:15:33 +0000

    Updates

--- assets/md.transition-legacy-sPRD7Fpx.js
+++ assets/md.transition-legacy-sPRD7Fpx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy--BTOFC-x.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-YB-AE8iO.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
