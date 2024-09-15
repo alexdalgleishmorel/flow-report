b9b9c510cfd15929640db85a4befaabe465c34a1
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-09-15 16:18:46 +0000

    Updates

--- assets/md.transition-legacy-EPKTMLS6.js
+++ assets/md.transition-legacy-EPKTMLS6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-wBj4wSOj.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-FNwIXHX8.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
