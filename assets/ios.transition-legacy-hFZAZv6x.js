c9862c21358d3ef376dbf27ba9cb8d12882e16d1
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-09-16 02:52:07 +0000

    Updates

--- assets/ios.transition-legacy-hFZAZv6x.js
+++ assets/ios.transition-legacy-hFZAZv6x.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-uX7ELU4Q.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-R-8Vtbsw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
