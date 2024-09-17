6b1a85670ad416121890fb8f3acf77ffa234461a
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-09-17 09:16:11 +0000

    Updates

--- assets/swipe-back-legacy-Sd567VGP.js
+++ assets/swipe-back-legacy-Sd567VGP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-HyinE5rY.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Rvyd8WQd.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
