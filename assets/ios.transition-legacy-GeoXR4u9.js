8d00d2902ba86214ae04989237ea3ed06023164a
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-09-18 22:15:15 +0000

    Updates

--- assets/ios.transition-legacy-GeoXR4u9.js
+++ assets/ios.transition-legacy-GeoXR4u9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-A0Y51bUu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-oFXoyUqZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
