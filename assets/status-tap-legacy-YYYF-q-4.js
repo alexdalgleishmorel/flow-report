4481a1ad082d4eb008a01feecc41d56194e15563
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-09-19 10:17:00 +0000

    Updates

--- assets/status-tap-legacy-YYYF-q-4.js
+++ assets/status-tap-legacy-YYYF-q-4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-azjdcGXi.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy--K882MFk.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
