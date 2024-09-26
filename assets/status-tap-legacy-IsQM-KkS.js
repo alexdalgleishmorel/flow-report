4375b9e8b3b111262eacd6bc02a2a3cb2b6bac88
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-09-26 08:20:57 +0000

    Updates

--- assets/status-tap-legacy-IsQM-KkS.js
+++ assets/status-tap-legacy-IsQM-KkS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fV81QIAO.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-JWHHS8aa.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
