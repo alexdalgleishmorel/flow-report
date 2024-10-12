122a557dfe9ce130bff5710e18b369fe8b4a9f01
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-10-12 10:15:20 +0000

    Updates

--- assets/ios.transition-legacy--ePDtquu.js
+++ assets/ios.transition-legacy--ePDtquu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-LuN1gFDK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-9Ce9rE1d.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
