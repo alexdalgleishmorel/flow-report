202ad9e42898d7180f60c04d971e286108c79464
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-10-17 17:14:31 +0000

    Updates

--- assets/md.transition-legacy-geAc_eMU.js
+++ assets/md.transition-legacy-geAc_eMU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Upj8j-5k.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-3a6u8v3m.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
