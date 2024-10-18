2d67e20ddf409954ae627dbed8735ac3be4915b2
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-10-18 22:15:10 +0000

    Updates

--- assets/ios.transition-legacy-CshPotMS.js
+++ assets/ios.transition-legacy-CshPotMS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CpsynxOF.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy--gh10fND.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
