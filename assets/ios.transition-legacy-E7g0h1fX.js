af0f39d92c12d4ac766c57b97cd819744f3f3d9d
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-10-29 03:30:31 +0000

    Updates

--- assets/ios.transition-legacy-E7g0h1fX.js
+++ assets/ios.transition-legacy-E7g0h1fX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-LdsXYP4T.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-m5Rmw_0R.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-ymiA2i2T.js
+++ assets/swipe-back-legacy-ymiA2i2T.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-LdsXYP4T.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-m5Rmw_0R.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
