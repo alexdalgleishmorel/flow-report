415a405374010f46f2894ea38426576f51946a9d
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-03 03:26:04 +0000

    Updates

--- assets/swipe-back-legacy-LyrKfGUV.js
+++ assets/swipe-back-legacy-LyrKfGUV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-SpprhRyq.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-dp1pRyou.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
