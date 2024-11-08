1411f363b0e1cb6f2de8388f44bfd005807418b2
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-08 20:16:58 +0000

    Updates

--- assets/md.transition-legacy-VnN-CYF2.js
+++ assets/md.transition-legacy-VnN-CYF2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-14j0ZlkJ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-VlOxPPVZ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
