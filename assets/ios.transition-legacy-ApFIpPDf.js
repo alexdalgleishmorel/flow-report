465737f52295d690437f02d89a342e9c50d55779
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-12 23:15:23 +0000

    Updates

--- assets/ios.transition-legacy-ApFIpPDf.js
+++ assets/ios.transition-legacy-ApFIpPDf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-L2oZqcXV.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-YzSepk6e.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
