c2b36c5a2b3589b7985fde55f7784ac759c7648b
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-16 12:29:57 +0000

    Updates

--- assets/ios.transition-legacy-qSuPI9ex.js
+++ assets/ios.transition-legacy-qSuPI9ex.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-2f-GLplM.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-48Z0nFDK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
