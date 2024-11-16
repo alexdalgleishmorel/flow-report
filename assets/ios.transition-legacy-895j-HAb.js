bc06b548985139cd5b76bce8b0d0b23554b17dbc
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-16 14:13:55 +0000

    Updates

--- assets/ios.transition-legacy-895j-HAb.js
+++ assets/ios.transition-legacy-895j-HAb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-kCRIZrOo.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-h-q3MzXZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
