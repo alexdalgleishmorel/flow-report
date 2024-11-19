940f0de33b6d0325b24370996212175ddf48ef74
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-19 21:15:50 +0000

    Updates

--- assets/ios.transition-legacy-Lpz500Me.js
+++ assets/ios.transition-legacy-Lpz500Me.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-R9iXMOm3.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-JVyPMeRy.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
