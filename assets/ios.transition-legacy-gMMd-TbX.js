a42bde7300781db45baedba48037eea5d9a3aa39
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-22 09:18:19 +0000

    Updates

--- assets/ios.transition-legacy-gMMd-TbX.js
+++ assets/ios.transition-legacy-gMMd-TbX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-HE_rmD5s.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-2-BJM2U0.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
