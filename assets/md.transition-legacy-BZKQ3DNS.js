8174962c923f847b5fb276fd6e55f3fcaea1656a
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-23 10:16:09 +0000

    Updates

--- assets/md.transition-legacy-BZKQ3DNS.js
+++ assets/md.transition-legacy-BZKQ3DNS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-g6uyNyv_.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-lpGRg5T1.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
