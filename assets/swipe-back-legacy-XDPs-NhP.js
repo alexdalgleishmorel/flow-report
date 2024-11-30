c4f3d8b8c0aab3e1adb399a569ebdeb634dcac99
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-11-30 05:15:38 +0000

    Updates

--- assets/swipe-back-legacy-XDPs-NhP.js
+++ assets/swipe-back-legacy-XDPs-NhP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-tp-ekO3g.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-ISj1aZ5z.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
