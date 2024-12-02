551fd3e73a3c8f74d1a1707d2cfcb349b441e072
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-02 14:17:10 +0000

    Updates

--- assets/md.transition-legacy-_Grr-cs7.js
+++ assets/md.transition-legacy-_Grr-cs7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-kFOu289Q.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-FLbSbeQ9.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
