6d0fe02cbb61afc6a3e5c8eae09da9b5afd3a951
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-03 21:16:14 +0000

    Updates

--- assets/ios.transition-legacy-hPM1RkPB.js
+++ assets/ios.transition-legacy-hPM1RkPB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-z_o85D2z.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-1oBh_ixz.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
