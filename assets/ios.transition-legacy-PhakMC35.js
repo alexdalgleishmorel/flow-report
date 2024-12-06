58bb79e1d73500a5619152b13aa631441a8c23f5
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-06 10:18:42 +0000

    Updates

--- assets/ios.transition-legacy-PhakMC35.js
+++ assets/ios.transition-legacy-PhakMC35.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-zK4D7Izz.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-uvUa0MQD.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
