c8130f699340a16c2fd79264ee60a5e52b01bdbf
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-07 08:20:41 +0000

    Updates

--- assets/swipe-back-legacy-klicSs3c.js
+++ assets/swipe-back-legacy-klicSs3c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-V3MLdIdw.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-IId_5Au-.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
