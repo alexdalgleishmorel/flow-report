02aa10811583292dc83097167449276deb83e928
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-11 11:14:30 +0000

    Updates

--- assets/md.transition-legacy-d6BO_RZM.js
+++ assets/md.transition-legacy-d6BO_RZM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-l88a8FtY.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-H4NsBCjA.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
