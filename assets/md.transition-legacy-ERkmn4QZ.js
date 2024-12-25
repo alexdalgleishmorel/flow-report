dd8896b4168eb14e5f476eaa619fc2769d979ea7
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-25 06:22:43 +0000

    Updates

--- assets/md.transition-legacy-ERkmn4QZ.js
+++ assets/md.transition-legacy-ERkmn4QZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ci8FqfOE.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-BrxiDB_p.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
