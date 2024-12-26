287f22a56fd8dc33771b80d399f0364070b5770b
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-26 17:13:38 +0000

    Updates

--- assets/ios.transition-legacy-Il1M-pga.js
+++ assets/ios.transition-legacy-Il1M-pga.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-7XrFv9DK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-5NbuF4vt.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
