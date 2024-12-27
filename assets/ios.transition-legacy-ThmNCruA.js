4a84ffeabf3610cb46778cc11dea4771c9263764
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-27 13:21:00 +0000

    Updates

--- assets/ios.transition-legacy-ThmNCruA.js
+++ assets/ios.transition-legacy-ThmNCruA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-aPcMBmOS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-rqvrwunM.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
