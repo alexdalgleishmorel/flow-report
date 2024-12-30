31d6dced487f1bc7f6f54439ce5ed783ed63198f
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-30 17:13:57 +0000

    Updates

--- assets/ios.transition-legacy-i6-LmCEN.js
+++ assets/ios.transition-legacy-i6-LmCEN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ASBvFuQq.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-64VW_-oE.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
