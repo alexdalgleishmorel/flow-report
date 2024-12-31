b3a41a5046bae32f43ab32e0b56866edbd435f20
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-12-31 03:19:47 +0000

    Updates

--- assets/md.transition-legacy-ZMPa3Wce.js
+++ assets/md.transition-legacy-ZMPa3Wce.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Zydf2B3t.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-o-i-Pe7q.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
