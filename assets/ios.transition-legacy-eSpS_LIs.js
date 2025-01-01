2cddf9314d929c1acec51dc391eb089e746c65d7
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-01 10:16:44 +0000

    Updates

--- assets/ios.transition-legacy-eSpS_LIs.js
+++ assets/ios.transition-legacy-eSpS_LIs.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-XlNSPGgA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-MYMUUR5T.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
