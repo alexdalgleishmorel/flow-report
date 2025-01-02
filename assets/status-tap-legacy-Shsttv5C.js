40ecc026ed461688244ac168824adc0758b68407
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-02 03:19:48 +0000

    Updates

--- assets/status-tap-legacy-Shsttv5C.js
+++ assets/status-tap-legacy-Shsttv5C.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-QP_iCKMU.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-OIwrKF-c.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
