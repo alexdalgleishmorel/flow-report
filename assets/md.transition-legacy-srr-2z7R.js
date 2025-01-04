21054bd4b1b3930cff618a4ceb483b24d2d3cc89
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-04 11:12:58 +0000

    Updates

--- assets/md.transition-legacy-srr-2z7R.js
+++ assets/md.transition-legacy-srr-2z7R.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-MmX2ldjZ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-mJxIdRG0.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DxT-gmOh.js
+++ assets/swipe-back-legacy-DxT-gmOh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-MmX2ldjZ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-mJxIdRG0.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
