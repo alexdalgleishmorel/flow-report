b8d3aceb47b818a4c21e20de66688403b6320dc3
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-04 18:20:17 +0000

    Updates

--- assets/md.transition-legacy--ogAPoF-.js
+++ assets/md.transition-legacy--ogAPoF-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-GrgMFOam.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-LlZE-h6d.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
