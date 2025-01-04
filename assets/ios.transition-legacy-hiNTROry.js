be715f34110bc1c433884477dec64aaed4048202
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-04 07:14:11 +0000

    Updates

--- assets/ios.transition-legacy-hiNTROry.js
+++ assets/ios.transition-legacy-hiNTROry.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-p20Depxd.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-ez_blOO3.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
