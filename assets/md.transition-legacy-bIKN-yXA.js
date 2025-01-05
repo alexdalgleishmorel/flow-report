a134be209ac38c3d5be69613328ae271117b3b81
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-05 03:24:00 +0000

    Updates

--- assets/md.transition-legacy-bIKN-yXA.js
+++ assets/md.transition-legacy-bIKN-yXA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-QYoFHi5Z.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-Q9lboWZy.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
