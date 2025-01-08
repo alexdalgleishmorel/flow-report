0a515af397931eb1208d90f1332d1d468b8011a7
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-08 21:14:48 +0000

    Updates

--- assets/ios.transition-legacy-Ewurqt-z.js
+++ assets/ios.transition-legacy-Ewurqt-z.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-c34RNB8G.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-HrK81NKO.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
