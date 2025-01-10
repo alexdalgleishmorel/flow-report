38bcaa0d14f651920c2c5c5da0bddbe21d5e52ed
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-10 16:20:21 +0000

    Updates

--- assets/md.transition-legacy-nVpPvLL2.js
+++ assets/md.transition-legacy-nVpPvLL2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-FluaOb_3.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-N0JG6O6k.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
