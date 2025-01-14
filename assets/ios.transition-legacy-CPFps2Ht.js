1a464cbd21cf735543692fc924cc0b2bf9ba9649
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-14 13:20:30 +0000

    Updates

--- assets/ios.transition-legacy-CPFps2Ht.js
+++ assets/ios.transition-legacy-CPFps2Ht.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Ju24KTrU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-abkXrgsw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
