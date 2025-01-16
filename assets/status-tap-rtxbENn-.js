2f2f55f84ce29f6760255e8d3af315e50daffa0b
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-16 03:30:37 +0000

    Updates

--- assets/status-tap-rtxbENn-.js
+++ assets/status-tap-rtxbENn-.js
@@ -1,3 +1,3 @@
-import{r as a,f as i,b as c,w as d,s as l}from"./index-yTShsZaL.js";/*!
+import{r as a,f as i,b as c,w as d,s as l}from"./index-bNoS2r1L.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
