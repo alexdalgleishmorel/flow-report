bba17deebd131a908c648fd014d10d0c40986c0f
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-17 00:11:19 +0000

    Updates

--- assets/md.transition-legacy-P0ixeVua.js
+++ assets/md.transition-legacy-P0ixeVua.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-a3jfldIs.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-iIM8rnJv.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
