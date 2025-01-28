fc83562e099c957d6d4b20526f4b04930e0bdcd3
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-01-28 12:30:36 +0000

    Updates

--- assets/ios.transition-legacy-zh-Hvkcp.js
+++ assets/ios.transition-legacy-zh-Hvkcp.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-OyVzei2F.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-SP1PaDGu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
