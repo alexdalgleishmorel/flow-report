daedb22a71108c99ceee2518527fda2b980c2fc0
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-02-02 23:16:34 +0000

    Updates

--- assets/md.transition-legacy-iPnfawOx.js
+++ assets/md.transition-legacy-iPnfawOx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-j4b9RxlE.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-0LzBkUab.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
