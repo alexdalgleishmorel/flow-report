51a820eae62cda62ec865772b5f835f37db8e016
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-02-04 16:20:07 +0000

    Updates

--- assets/status-tap-legacy--DfBMV6B.js
+++ assets/status-tap-legacy--DfBMV6B.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-t9bKnfjJ.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-YW1z9xDN.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-vbzxUjyB.js
+++ assets/status-tap-vbzxUjyB.js
@@ -1,3 +1,3 @@
-import{r as a,f as i,b as c,w as d,s as l}from"./index-Ce7EzOQx.js";/*!
+import{r as a,f as i,b as c,w as d,s as l}from"./index-K3EJx3UR.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
