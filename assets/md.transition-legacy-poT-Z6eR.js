976844a90010e1de387162cd20140427759b8726
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-02-06 07:15:36 +0000

    Updates

--- assets/md.transition-legacy-poT-Z6eR.js
+++ assets/md.transition-legacy-poT-Z6eR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-qOojzzmh.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-m9-Vh61E.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
