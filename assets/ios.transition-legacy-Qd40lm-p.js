3587263b7cdb2d87ea8af3e9a24b9b482936fea8
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-02-08 22:13:50 +0000

    Updates

--- assets/ios.transition-legacy-Qd40lm-p.js
+++ assets/ios.transition-legacy-Qd40lm-p.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ZVm1dXCP.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-PalHOXAa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
