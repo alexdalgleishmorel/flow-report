7ce50fcd9100451772ce08efc975cb3ce44ecb00
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-02-23 13:18:25 +0000

    Updates

--- assets/md.transition-legacy-ijhm2SSB.js
+++ assets/md.transition-legacy-ijhm2SSB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-UIUnymDT.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-bpxM2f7r.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
