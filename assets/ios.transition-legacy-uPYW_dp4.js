893f28fc2faf1773dbe9d7a14b3a7dd9a553b174
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-02 14:15:29 +0000

    Updates

--- assets/ios.transition-legacy-uPYW_dp4.js
+++ assets/ios.transition-legacy-uPYW_dp4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-hx5txQpX.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-kbkI7C4C.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
