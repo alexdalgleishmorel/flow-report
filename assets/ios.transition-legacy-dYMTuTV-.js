c3e7e7e16c9cc645299733082a1f104c8b9068b5
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-09 16:16:31 +0000

    Updates

--- assets/ios.transition-legacy-dYMTuTV-.js
+++ assets/ios.transition-legacy-dYMTuTV-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Nnn0kONS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-6xQfF4iK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
