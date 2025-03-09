3331dd14bbbab9e65604730d33514aab05f80213
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-09 03:16:07 +0000

    Updates

--- assets/ios.transition-legacy-hgptI_v8.js
+++ assets/ios.transition-legacy-hgptI_v8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-sB20XZy5.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-piADL4TM.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
