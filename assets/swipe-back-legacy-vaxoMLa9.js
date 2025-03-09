4bda134b6402a900ed358c1a3b5616bf4cc4318a
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-09 15:15:19 +0000

    Updates

--- assets/swipe-back-legacy-vaxoMLa9.js
+++ assets/swipe-back-legacy-vaxoMLa9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-F0B3rkhe.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Nnn0kONS.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
