f56794e37f8a2cdfd53cfafccd7a4f4364dd810b
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-14 12:32:05 +0000

    Updates

--- assets/swipe-back-legacy--MaGITOR.js
+++ assets/swipe-back-legacy--MaGITOR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy--FV0NuoI.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-gWoZqMZ7.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
