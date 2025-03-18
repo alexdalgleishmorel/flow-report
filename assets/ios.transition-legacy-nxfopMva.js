ab6edc7982dec572f9650d96f8ced7095bad1dc4
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-18 10:18:28 +0000

    Updates

--- assets/ios.transition-legacy-nxfopMva.js
+++ assets/ios.transition-legacy-nxfopMva.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-pwtqEHKA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-11hx0n8Q.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
