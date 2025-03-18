879abcd58d6c9ec53cbb594d3efc2617cfee246f
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-18 20:18:08 +0000

    Updates

--- assets/ios.transition-legacy-CMTLUKgI.js
+++ assets/ios.transition-legacy-CMTLUKgI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DGoxisos.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-ol_90AOf.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
