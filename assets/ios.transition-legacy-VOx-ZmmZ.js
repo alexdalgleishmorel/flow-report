63c9a30becd2b209bd97353e66fe42566e06c1b4
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-21 17:15:06 +0000

    Updates

--- assets/ios.transition-legacy-VOx-ZmmZ.js
+++ assets/ios.transition-legacy-VOx-ZmmZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-p_Yo9TG5.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-dfA78KXP.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
