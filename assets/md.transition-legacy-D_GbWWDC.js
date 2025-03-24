2151c438b329f2458cd9ca4eb44d7cb425b1c518
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-24 16:22:35 +0000

    Updates

--- assets/md.transition-legacy-D_GbWWDC.js
+++ assets/md.transition-legacy-D_GbWWDC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-8kC1q1z9.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-qe29cTAG.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
