cd994bd513a6576e24afc5978bbf30ff6ce8f586
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-03-25 08:22:47 +0000

    Updates

--- assets/ios.transition-legacy-l-TEMHbi.js
+++ assets/ios.transition-legacy-l-TEMHbi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-4BXCzPwE.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-zDEq992V.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
