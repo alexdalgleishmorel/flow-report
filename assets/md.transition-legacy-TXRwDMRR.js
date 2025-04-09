984f2d84ba16e1b558f8fe9011ea1e28ac93f313
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-04-09 13:28:43 +0000

    Updates

--- assets/md.transition-legacy-TXRwDMRR.js
+++ assets/md.transition-legacy-TXRwDMRR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-05X7aaPx.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-Yh-oESyu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
