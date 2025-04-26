62e16bc4fe41481283a7f7c94f83cc81402e7732
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-04-26 17:13:56 +0000

    Updates

--- assets/status-tap-legacy-PYVP_Iho.js
+++ assets/status-tap-legacy-PYVP_Iho.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-WQUj6l8a.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-hmMbjVbw.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
