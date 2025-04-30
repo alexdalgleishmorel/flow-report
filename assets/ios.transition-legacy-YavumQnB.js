906acf553824ce7d27b975bd86d764555d6e1214
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-04-30 15:19:13 +0000

    Updates

--- assets/ios.transition-legacy-YavumQnB.js
+++ assets/ios.transition-legacy-YavumQnB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-LLtbPxHy.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-UWVU-i_d.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
