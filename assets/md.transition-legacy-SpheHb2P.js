33302be9e041125a1d962b5866dd7f604e645f0e
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-05-09 07:17:52 +0000

    Updates

--- assets/md.transition-legacy-SpheHb2P.js
+++ assets/md.transition-legacy-SpheHb2P.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-4xaPWt6h.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-ZetvxjPx.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
