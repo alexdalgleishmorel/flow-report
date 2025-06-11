94700a08010253a5a20e11d528a497d0953dcc44
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-06-11 10:20:22 +0000

    Updates

--- assets/md.transition-legacy-sdtfSfJv.js
+++ assets/md.transition-legacy-sdtfSfJv.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BsMbdpMo.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-kXUyn1DG.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
