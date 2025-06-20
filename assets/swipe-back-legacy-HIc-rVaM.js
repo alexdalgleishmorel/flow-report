fef57a97a6de633b270a98373279b9298a960969
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-06-20 12:37:44 +0000

    Updates

--- assets/swipe-back-legacy-HIc-rVaM.js
+++ assets/swipe-back-legacy-HIc-rVaM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-d8I0W02R.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-g4R1dYX6.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
