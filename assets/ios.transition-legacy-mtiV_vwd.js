9ce40dc068686412847b7e4b727453d31eb4d8f6
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-06-22 16:21:10 +0000

    Updates

--- assets/ios.transition-legacy-mtiV_vwd.js
+++ assets/ios.transition-legacy-mtiV_vwd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-0JC5qwbu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-PjOGBrvm.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
