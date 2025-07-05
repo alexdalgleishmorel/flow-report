f4775377e50711c5092da154ad0934e1df1867a3
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-07-05 12:34:17 +0000

    Updates

--- assets/swipe-back-legacy-CI-2WILr.js
+++ assets/swipe-back-legacy-CI-2WILr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-gmglMTH7.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-L_ezqO3g.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
