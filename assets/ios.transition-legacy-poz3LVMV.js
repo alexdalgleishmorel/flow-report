998f05286c1e4ed64b889dbb2a9e46b76373132c
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2025-07-30 11:18:14 +0000

    Updates

--- assets/ios.transition-legacy-poz3LVMV.js
+++ assets/ios.transition-legacy-poz3LVMV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-P1TQoe3g.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-j2R47mAa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
